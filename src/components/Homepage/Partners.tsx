c9f845af55ec581320accf1bfce95adeedeb96f2
Eduardo Ribeiro <eduardo.ribeiro@outlook.pt>
2022-11-02 02:48:07 +0000

    Correct logos

--- src/components/Homepage/Partners.tsx
+++ src/components/Homepage/Partners.tsx
@@ -1,29 +1,28 @@
-import { Typography, Grid, Box } from "@mui/material"
+import { Typography, Grid, Box, Stack } from "@mui/material"
 import React from "react"
 import { PartnersLogos } from "../../content/partners"
 import theme from "../../theme"
 import PartnerLogo from "../logos"
 
 export interface PartnersProps {
-    partnersData: PartnersLogos[];
+  partnersData: PartnersLogos[]
 }
 
-const Partners = ({partnersData}:PartnersProps) => (
+const Partners = ({ partnersData }: PartnersProps) => (
   <Box>
     <Typography variant="h6" sx={{ margin: theme.spacing(8, 0, 1, 0) }}>
       PARCEIROS
     </Typography>
-    <Grid container spacing={2} sx={{ padding: theme.spacing(2, 0) }}>
+    {/* <Grid container spacing={2} sx={{ padding: theme.spacing(2, 0) }}> */}
+    <Stack direction="row" spacing={2}>
       {partnersData.map(partner => (
-        <Grid item xs={2.4}>
-          <PartnerLogo
-            key={partner.image}
-            image={partner.image}
-            alt={partner.alt}
-          />
-        </Grid>
+        <PartnerLogo
+          key={partner.image}
+          image={partner.image}
+          alt={partner.alt}
+        />
       ))}
-    </Grid>
+    </Stack>
   </Box>
 )
 
--- src/components/logos.tsx
+++ src/components/logos.tsx
@@ -4,19 +4,19 @@ import { PartnersLogos } from "../content/partners"
 import Card from "@mui/material/Card"
 import { CardActionArea } from "gatsby-theme-material-ui"
 import CardContent from "@mui/material/CardContent"
-import Img from "gatsby-image"
+import theme from "../theme"
 
 const PartnerLogo = ({
   image,
-  width = 48,
+  width = 'auto',
   quality = 100,
     alt = "",
   link
 }: PartnersLogos) => (
   <Card>
-    <CardActionArea>
-      <CardContent>
-        <img src={image} alt={alt} title={alt} />
+    <CardActionArea sx={{height: 100}}>
+      <CardContent sx={{display: 'flex', maxHeight: 96, justifyContent: 'center', alignItems: 'center', padding: theme.spacing(2)}}>
+        <img src={image} alt={alt} title={alt} width={width} style={{maxHeight: 100}} />
       </CardContent>
     </CardActionArea>
   </Card>
