7a3b588bbbe55ee0a7af51f78fc644c09c62849a
Eduardo Ribeiro <eduardo.ribeiro@outlook.pt>
2022-11-06 19:24:34 +0000

    Correct text color

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,19 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles"
 
 const theme = createTheme({
-    palette: {
-        mode: 'dark',
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#8a6534",
     },
-});
+    secondary: {
+      main: "#f50057",
+    },
+    text: {
+      primary: "#F3C057",
+      secondary: "#E7F6F2",
+    },
+  },
+})
 
-export default theme;
+export default theme
